54f9063f3893484c6d4695935b79c2173a24f19c
Dwayne Redwood <87104481+coderedwood@users.noreply.github.com>
2022-09-08 22:28:00 -0500

    Boiler plate code plus window label

--- pythonui.py
+++ pythonui.py
@@ -1,2 +1,11 @@
 ##############################
 #to create logic for ui layout#
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=200)
+frm.grid()
+root.title('Pythongui')
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
